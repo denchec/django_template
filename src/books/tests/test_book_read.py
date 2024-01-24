1cb87447493ca905c076f9ae0724493003c06efe
BrittleFoot <buftioo@gmail.com>
2024-01-24 23:11:12 +0400

    embrace faker

--- src/books/tests/test_book_read.py
+++ src/books/tests/test_book_read.py
@@ -1,22 +1,23 @@
 import pytest
+from books.models import Author, Book
 
 pytestmark = [pytest.mark.django_db]
 
 
 @pytest.fixture
-def author(mixer):
-    return mixer.blend("books.Author", name="J. K. Rowling")
+def author(mixer) -> Author:
+    return mixer.blend(Author)
 
 
 @pytest.fixture
-def book(mixer, author):
-    return mixer.blend("books.Book", name="Harry Potter", author=author)
+def book(mixer, author) -> Book:
+    return mixer.blend(Book, author=author)
 
 
 def test_book_read(api, book, author):
     result = api.get("/api/v1/books/")
 
     assert result[0]["id"] == book.id
-    assert result[0]["name"] == "Harry Potter"
+    assert result[0]["name"] == book.name
     assert result[0]["author"]["id"] == author.id
-    assert result[0]["author"]["name"] == "J. K. Rowling"
+    assert result[0]["author"]["name"] == author.name
