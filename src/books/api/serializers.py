d8b3cb412228641aa3f427bca705141b33173880
BrittleFoot <buftioo@gmail.com>
2024-01-24 21:40:52 +0400

    api

---
+++ src/books/api/serializers.py
@@ -0,0 +1,17 @@
+
+from rest_framework import serializers
+from books.models import Author, Book
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = ["id", "name"]
+
+
+class BookSerializer(serializers.ModelSerializer):
+    author = AuthorSerializer()
+
+    class Meta:
+        model = Book
+        fields = ["id", "name", "author"]
---
+++ src/books/tests/test_book_read.py
@@ -0,0 +1,23 @@
+import pytest
+
+
+pytestmark = [pytest.mark.django_db]
+
+
+@pytest.fixture
+def author(mixer):
+    return mixer.blend("books.Author", name="J. K. Rowling")
+
+
+@pytest.fixture
+def book(mixer, author):
+    return mixer.blend("books.Book", name="Harry Potter", author=author)
+
+
+def test_book_read(api, book, author):
+    result = api.get("/api/v1/books/")
+
+    assert result[0]["id"] == book.id
+    assert result[0]["name"] == "Harry Potter"
+    assert result[0]["author"]["id"] == author.id
+    assert result[0]["author"]["name"] == "J. K. Rowling"
